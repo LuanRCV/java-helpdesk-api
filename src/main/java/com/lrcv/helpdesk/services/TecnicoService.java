67df576cca3f6ec1890a353bcced1adb58d458d5
LuanRCV <luan.vieira@stone.com.br>
2022-03-02 21:41:31 -0300

    Finishing Tecnicos domain CRUD with update and delete

--- src/main/java/com/lrcv/helpdesk/services/TecnicoService.java
+++ src/main/java/com/lrcv/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.lrcv.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -41,6 +43,26 @@ public class TecnicoService {
 		Tecnico tecnico = new Tecnico(tecnicoObjDTO);
 
 		return repository.save(tecnico);
+	}
+
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		validaPorCpfEEmail(objDTO);
+
+		Tecnico obj = findById(id);
+		obj = new Tecnico(objDTO);
+
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado");
+		}
+
+		repository.deleteById(id);
 	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
